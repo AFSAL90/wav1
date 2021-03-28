93a0ba96d2f16b3a271cdfb75555445635270da3
AFSAL90 <80841484+AFSAL90@users.noreply.github.com>
2021-03-29 00:19:16 +0530

    add luaser 😌✅

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('BLACK THUNDER', {
+CFonts.say('BLACK THUNDER & LUA SER', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
